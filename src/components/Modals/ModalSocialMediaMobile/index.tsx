b0d93db64558a09ed92f87f6da9a7ec07c4500d0
ChristianDaniel <christian_daniel04@hotmail.com>
2022-09-13 11:07:11 -0300

    refactor: change Modal SocialMedia links for links institutional pages

--- src/components/Modals/ModalSocialMediaMobile/index.tsx
+++ src/components/Modals/ModalSocialMediaMobile/index.tsx
@@ -1,13 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-
-import discord from '../../../../public/assets/socialMidia/discord.svg'
-import telegram from '../../../../public/assets/socialMidia/telegram.svg'
-import twitter from '../../../../public/assets/socialMidia/twitter.svg'
-import medium from '../../../../public/assets/socialMidia/medium.svg'
-import github from '../../../../public/assets/socialMidia/github.svg'
-import kacyWhite from '../../../../public/assets/logos/kacy-white.svg'
+import Link from 'next/link'
 
 import * as S from './styles'
 
@@ -15,60 +7,62 @@ interface IModalSocialMediaMobileProps {
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const ModalSocialMediaMobile = ({
-  setModalOpen
-}: IModalSocialMediaMobileProps) => {
-  const router = useRouter()
+// eslint-disable-next-line prettier/prettier
+const ModalSocialMediaMobile = ({ setModalOpen }: IModalSocialMediaMobileProps) => {
   return (
     <>
       <S.Backdrop onClick={() => setModalOpen(false)} />
       <S.ModalContainer>
-        <S.SocialIcon
-          href="https://discord.gg/fAqpbP6tFw"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => setModalOpen(false)}
-        >
-          <Image src={discord} alt="discord" />
-          <span>Discord</span>
-        </S.SocialIcon>
-        <S.SocialIcon
-          href="https://t.me/KassandraDAO"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => setModalOpen(false)}
-        >
-          <Image src={telegram} alt="telegram" />
-          <span>Telegram</span>
-        </S.SocialIcon>
-        <S.SocialIcon
-          href="https://twitter.com/dao_kassandra"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => setModalOpen(false)}
-        >
-          <Image src={twitter} alt="twitter" />
-          <span>Twitter</span>
-        </S.SocialIcon>
-        <S.SocialIcon
-          className="medium"
-          href="https://kassandrafoundation.medium.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => setModalOpen(false)}
-        >
-          <Image className="medium" src={medium} alt="medium" />
-          <span>Medium</span>
-        </S.SocialIcon>
-        <S.SocialIcon
-          href="https://github.com/KassandraFinance"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => setModalOpen(false)}
-        >
-          <Image src={github} alt="Github" />
-          <span>Github</span>
-        </S.SocialIcon>
+        <Link href="https://kassandra.finance/" passHref>
+          <S.SocialIcon
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>Home</span>
+          </S.SocialIcon>
+        </Link>
+        <Link href="https://kassandra.finance/investors" passHref>
+          <S.SocialIcon
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>Investors</span>
+          </S.SocialIcon>
+        </Link>
+
+        <Link href="https://kassandra.finance/managers" passHref>
+          <S.SocialIcon
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>Managers</span>
+          </S.SocialIcon>
+        </Link>
+        <Link href="https://kassandra.finance/dao" passHref>
+          <S.SocialIcon
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>DAO</span>
+          </S.SocialIcon>
+        </Link>
+        <Link href="https://kassandra.finance/foundation" passHref>
+          <S.SocialIcon
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>Foundation</span>
+          </S.SocialIcon>
+        </Link>
+        <Link href="https://kassandrafoundation.medium.com/" passHref>
+          <S.SocialIcon
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => setModalOpen(false)}
+          >
+            <span>Blog</span>
+          </S.SocialIcon>
+        </Link>
       </S.ModalContainer>
     </>
   )
