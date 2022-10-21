3e1fde17cfe6d32d93ccc290569943605343246d
ChristianDaniel <christian_daniel04@hotmail.com>
2022-10-20 23:54:57 -0300

    refactor: change redux for redux custom context​

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,30 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import type { RootState, AppDispatch } from '.'
+import React from 'react'
+import { TypedUseSelectorHook } from 'react-redux'
+import { createDispatchHook } from 'react-redux'
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+import { ReduxContext } from './reduxContext'
+import { RootState, store } from '.'
+
+export const useAppDispatch = createDispatchHook(ReduxContext)
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = selector => {
+  const [, forceUpdate] = React.useReducer(c => c + 1, 0)
+  const currentState = React.useRef<any>()
+  currentState.current = selector(store.getState())
+
+  React.useEffect(() => {
+    return store.subscribe(() => {
+      try {
+        const nextState = selector(store.getState())
+        if (nextState === currentState.current) {
+          return
+        }
+      } catch (err) {
+        console.log(err)
+      }
+      forceUpdate()
+    })
+  }, [store, forceUpdate, selector, currentState])
+
+  return currentState.current
+}
