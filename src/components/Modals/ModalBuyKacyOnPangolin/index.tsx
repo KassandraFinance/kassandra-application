404a78f0d06215a5e3d47319ec84c7ef1d7a4085
ChristianDaniel <christian_daniel04@hotmail.com>
2022-10-24 16:31:44 -0400

    refactor: change provider walletConnect in modalbuyKacyOnPangolin component

--- src/components/Modals/ModalBuyKacyOnPangolin/index.tsx
+++ src/components/Modals/ModalBuyKacyOnPangolin/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
-import type {
+import Web3 from 'web3';
+import {
   SwapWidget as SwapWidgetType,
   PangolinProvider as PangolinProviderType
 } from '@pangolindex/components';
@@ -13,8 +14,7 @@ import { Kacy } from '../../../constants/tokenAddresses'
 import Button from '../../Button'
 import ModalWalletConnect from '../ModalWalletConnect'
 
-import Web3Modal from 'web3modal';
-import web3, { provider } from '../../../utils/web3'
+import { provider } from '../../../utils/web3';
 
 import spinerIcon from '../../../../public/assets/iconGradient/spinner.png'
 
@@ -27,7 +27,6 @@ ssr: false,
 }) as typeof SwapWidgetType;
 
 const PangolinProvider = dynamic(
-  // eslint-disable-next-line prettier/prettier
   () => import('@pangolindex/components').then((module) => module.PangolinProvider) as any,
   { ssr: false },
 ) as typeof PangolinProviderType;
@@ -42,45 +41,11 @@ const ModalBuyKacyOnPangolin = ({
   setModalOpen
 }: IModalBuyKacyOnPangolinProps) => {
   const [isModalWallet, setIsModaWallet] = React.useState<boolean>(false)
-  const [web3Modal, setWeb3Modal] = React.useState<Web3Modal>()
-  const [web3ModalProvider, setWeb3ModalProvider] = React.useState()
 
   const userWalletAddress = useAppSelector(state => state.userWalletAddress)
   const chainId = useAppSelector(state => state.chainId)
 
-  React.useEffect(() => {
-    const web3Modal = new Web3Modal({
-      cacheProvider: true,
-      providerOptions: {
-        injected: {
-          display: {
-            logo: 'data:image/gif;base64,INSERT_BASE64_STRING',
-            name: 'Injected',
-            description: 'Connect with the provider in your Browser',
-          },
-          package: null,
-        },
-      },
-    });
-
-    setWeb3Modal(web3Modal);
-  }, []);
-
-  const connectWallet = async () => {
-    try {
-      const web3provider = await web3Modal?.connect();
-      setWeb3ModalProvider(web3provider)
-    } catch (error: any) {
-      console.log(error);
-    }
-  };
-
-  React.useEffect(() => {
-    if (web3Modal?.cachedProvider && userWalletAddress) {
-      connectWallet();
-    }
-  }, [web3Modal, userWalletAddress]);
-
+  const walletProvider = new Web3(provider as any);
 
   function handleCloseModal() {
     setModalOpen(false)
@@ -113,7 +78,7 @@ const ModalBuyKacyOnPangolin = ({
         </S.LoadingContent>
       </S.LoadingContainer>
       {userWalletAddress !== '' && (
-        <PangolinProvider account={userWalletAddress} chainId={chainId} library={web3ModalProvider} theme={swapTheme}>
+        <PangolinProvider account={userWalletAddress} chainId={chainId} library={walletProvider.givenProvider} theme={swapTheme}>
             <S.ModalBuyKacyContainer modalOpen={modalOpen}>
               <SwapWidget isLimitOrderVisible={false} defaultOutputToken={Kacy} />
             </S.ModalBuyKacyContainer>
