98e7380efc031f4feef876949ace5588b79ef802
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-03 20:56:01 -0400

    feat: add TokenWithNetworkImage in product page

--- src/templates/Products/index.tsx
+++ src/templates/Products/index.tsx
@@ -35,9 +35,11 @@ import ChartProducts from '../../components/ChartProducts'
 import PoolOperations from '../../components/PoolOperations'
 import ScrollUpButton from '../../components/ScrollUpButton'
 import BreadcrumbItem from '../../components/Breadcrumb/BreadcrumbItem'
+import TokenWithNetworkImage from '../../components/TokenWithNetworkImage'
 import PoweredBy from './PoweredBy'
 
 import tooltip from '../../../public/assets/utilities/tooltip.svg'
+import avax from '../../../public/assets/logos/avax.png'
 
 import { GET_INFO_POOL } from './graphql'
 
@@ -168,7 +170,8 @@ const Products = ({ product }: Input) => {
   )
 
   const { data: coinGeckoResponse } = useSWR(
-    `/api/image-coingecko?poolinfo=${network2coingeckoID[product.platform]
+    `/api/image-coingecko?poolinfo=${
+      network2coingeckoID[product.platform]
     }&tokenAddress=${product.addresses}`
   )
 
@@ -275,7 +278,7 @@ const Products = ({ product }: Input) => {
             ),
             image:
               coinGeckoResponse.images[
-              invertToken[item.token.id] ?? item.token.id
+                invertToken[item.token.id] ?? item.token.id
               ]
           }
         }
@@ -390,7 +393,20 @@ const Products = ({ product }: Input) => {
       ) : (
         <>
           <S.Intro introMobile={true} introDesktop={false}>
-            <Image src={product.fundIcon} alt="" width={75} height={75} />
+            <TokenWithNetworkImage
+              tokenImage={{
+                url: product.fundIcon.src,
+                height: 75,
+                width: 75,
+                withoutBorder: true
+              }}
+              networkImage={{
+                url: avax.src,
+                height: 24,
+                width: 24,
+                withoutBorder: true
+              }}
+            />
             <S.NameIndex>
               <S.NameAndSymbol introMobile={true}>
                 <h1>{product.name}</h1>
@@ -418,7 +434,20 @@ const Products = ({ product }: Input) => {
           <S.Product>
             <S.ProductDetails>
               <S.Intro introMobile={false} introDesktop={true}>
-                <Image src={product.fundIcon} alt="" width={75} height={75} />
+                <TokenWithNetworkImage
+                  tokenImage={{
+                    url: product.fundIcon.src,
+                    height: 75,
+                    width: 75,
+                    withoutBorder: true
+                  }}
+                  networkImage={{
+                    url: avax.src,
+                    height: 24,
+                    width: 24,
+                    withoutBorder: true
+                  }}
+                />
                 <S.NameIndex>
                   <S.NameAndSymbol>
                     <h1>{product.name}</h1>
