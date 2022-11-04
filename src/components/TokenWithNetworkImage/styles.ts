b52943a6f676454c5f15dddcc9486e04fe6fe29b
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-03 20:55:22 -0400

    feat: create TokenWithNetworkImage component

---
+++ src/components/TokenWithNetworkImage/styles.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import styled, { css } from 'styled-components'
+
+interface IwithoutBorderProps {
+  withoutBorder?: boolean;
+}
+
+export const TokenWithNetworkImage = styled.div<IwithoutBorderProps>`
+  ${({ withoutBorder }) => css`
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${!withoutBorder ? '0.6rem' : '0'};
+
+    background-color: ${!withoutBorder && '#211426'};
+    border-radius: 50%;
+  `}
+`
+
+export const networkImageContainer = styled.span<IwithoutBorderProps>`
+  ${({ withoutBorder }) => css`
+    position: absolute;
+    bottom: ${!withoutBorder ? '-0.5rem' : '-0.1rem'};
+    right: ${!withoutBorder ? '-0.5rem' : '0.3rem'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${!withoutBorder ? '0.3rem' : '0'};
+
+    background-color: ${!withoutBorder && '#211426'};
+    border-radius: 50%;
+  `}
+`
