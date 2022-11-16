aece183527832231e4648c30df0e70cf1567b65c
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:48:49 -0400

    feat: create TokenAssetOut component

---
+++ src/components/NewPoolOperations/Form/TokenAssetOut/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const TokenAssetOut = () => {
+  return <S.TokenAssetOut>TokenAssetOut</S.TokenAssetOut>
+}
+
+export default TokenAssetOut
---
+++ src/components/NewPoolOperations/Form/TokenAssetOut/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components'
+
+export const TokenAssetOut = styled.div`
+  ${({ theme }) => css`
+    font-size ${theme.font.sizes.font16};
+  `}
+`
