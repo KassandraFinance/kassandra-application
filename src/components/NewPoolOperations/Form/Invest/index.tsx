3c76d4ee36f7e024bfff4a43ae42513e4d94024f
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:47:19 -0400

    feat: create Invest component

---
+++ src/components/NewPoolOperations/Form/Invest/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import InputAndOutputValueToken from '../InputAndOutputValueToken'
+
+import * as S from './styles'
+
+const Invest = () => {
+  // function handleSubmit() {
+
+  // }
+  return (
+    <S.Invest>
+      <InputAndOutputValueToken />
+    </S.Invest>
+  )
+}
+
+export default Invest
---
+++ src/components/NewPoolOperations/Form/Invest/styles.ts
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components'
+
+export const Invest = styled.form`
+  /* ${({ theme }) => css`
+    background-color: ${theme.colors.blue};
+  `} */
+
+  max-height: 100%;
+  padding-top: 2rem;
+  padding-right: 3.2rem;
+  padding-bottom: 3.2rem;
+  padding-left: 3.2rem;
+
+  /* .btn-submit {
+    margin-top: 2rem;
+  }
+
+  @media (max-width: 500px) {
+    padding: 2rem;
+  } */
+`
