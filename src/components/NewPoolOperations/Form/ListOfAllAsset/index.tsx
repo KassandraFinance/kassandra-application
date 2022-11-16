cb3a103f8575f29826a560b9b1630d8463d945e5
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:47:47 -0400

    feat: create ListOfAllAsset component

---
+++ src/components/NewPoolOperations/Form/ListOfAllAsset/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const ListOfAllAsset = () => {
+  return <S.ListOfAllAsset>ListOfAllAsset</S.ListOfAllAsset>
+}
+
+export default ListOfAllAsset
---
+++ src/components/NewPoolOperations/Form/ListOfAllAsset/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components'
+
+export const ListOfAllAsset = styled.form`
+  ${({ theme }) => css`
+    font-size ${theme.font.sizes.font16};
+  `}
+`
