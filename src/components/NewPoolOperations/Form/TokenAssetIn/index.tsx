6ad42ad29397b01a0ae3ab84e5dd7e0ca1f2b592
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:48:13 -0400

    feat: create TokenAssetIn component

---
+++ src/components/NewPoolOperations/Form/TokenAssetIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const TokenAssetIn = () => {
+  return <S.TokenAssetIn>TokenAssetIn</S.TokenAssetIn>
+}
+
+export default TokenAssetIn
---
+++ src/components/NewPoolOperations/Form/TokenAssetIn/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components'
+
+export const TokenAssetIn = styled.form`
+  ${({ theme }) => css`
+    font-size ${theme.font.sizes.font16};
+  `}
+`
