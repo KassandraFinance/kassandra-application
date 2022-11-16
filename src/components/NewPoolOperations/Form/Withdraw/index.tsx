0ee338c84d36b4d4eb4a49f223a8108a18ee0822
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:50:01 -0400

    feat: create Withdraw component

---
+++ src/components/NewPoolOperations/Form/Withdraw/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Withdraw = () => {
+  return (
+    <S.Withdraw>
+      <span>Withdraw</span>
+    </S.Withdraw>
+  )
+}
+
+export default Withdraw
---
+++ src/components/NewPoolOperations/Form/Withdraw/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components'
+
+export const Withdraw = styled.form`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.darkGray};
+  `}
+`
