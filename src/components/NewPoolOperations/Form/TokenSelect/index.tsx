d6535d2ca61af3479d1230da3585f1ec9284737c
Jony <jonyreiscardoso@gmail.com>
2022-11-16 07:49:13 -0400

    feat: create TokenSelect component

---
+++ src/components/NewPoolOperations/Form/TokenSelect/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const TokenSelect = () => {
+  return <S.TokenSelect>TokenSelect</S.TokenSelect>
+}
+
+export default TokenSelect
---
+++ src/components/NewPoolOperations/Form/TokenSelect/styles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from 'styled-components'
+
+export const TokenSelect = styled.div`
+  ${({ theme }) => css`
+    font-size ${theme.font.sizes.font16};
+  `}
+`
