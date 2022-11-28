89b8f4a47a901a7af16c7f1aff7b28b78b2b4650
ChristanDaniel <christian_daniel04@hotmail.com>
2022-11-28 19:45:52 -0400

    refactor: eslint and prettier format file TokenSelection

--- src/components/NewPoolOperations/Form/TokenSelection/index.tsx
+++ src/components/NewPoolOperations/Form/TokenSelection/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FixedSizeList as List } from 'react-window'
 import { stringSimilarity } from 'string-similarity-js'
 import { isAddress } from 'web3-utils'
 import BigNumber from 'bn.js'
@@ -15,19 +14,6 @@ import { BNtoDecimal } from '../../../../utils/numerals'
 
 import * as S from './styles'
 
-export interface IUserTokenProps extends ITokenList1InchProps {
-  balanceInDollar: string;
-  balance: string;
-}
-interface ITokenSelectionProps {
-  tokenList1Inch: ITokenList1InchProps[];
-}
-
-interface ICurrencyRowProps {
-  index: number;
-  style: React.CSSProperties;
-}
-
 export type IListbalanceTokenprops = {
   [key: string]: {
     balance: number
@@ -40,53 +26,85 @@ export type IListTokenPricesprops = {
   }
 }
 
+export interface IUserTokenProps extends ITokenList1InchProps {
+  balanceInDollar: string;
+  balance: string;
+}
+interface ITokenSelectionProps {
+  tokenList1Inch: ITokenList1InchProps[];
+}
+
 const URL_COINGECKO = 'https://api.coingecko.com/api/v3'
 const URL_1INCH_BALANCE = 'https://balances.1inch.io/v1.1'
 
 const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
   const [searchToken, setSearchToken] = React.useState('')
-  const [balanceToken, setBalanceToken] = React.useState<IListbalanceTokenprops>({['']: {
-    balance: 0
-  }})
-  const [listTokenPrices, setlistTokenPrices] = React.useState<IListTokenPricesprops>({['']: {
-    usd: 0
-  }})
+  const [balanceToken, setBalanceToken] =
+    React.useState<IListbalanceTokenprops>({
+      ['']: {
+        balance: 0
+      }
+    })
+  const [listTokenPrices, setlistTokenPrices] =
+    React.useState<IListTokenPricesprops>({
+      ['']: {
+        usd: 0
+      }
+    })
 
   const dispatch = useAppDispatch()
   const { userWalletAddress } = useAppSelector(state => state)
 
   async function handleFetchTokenPrice() {
-    const tokenPrice = tokenList1Inch.reduce((addressAccumulator, tokenCurrent) => addressAccumulator + (tokenCurrent.address + ','), '')
+    const tokenPrice = tokenList1Inch.reduce(
+      (addressAccumulator, tokenCurrent) =>
+        addressAccumulator + (tokenCurrent.address + ','),
+      ''
+    )
 
-    const response = await fetch(`${URL_COINGECKO}/simple/token_price/avalanche?contract_addresses=${tokenPrice}&vs_currencies=usd`)
+    const response = await fetch(
+      `${URL_COINGECKO}/simple/token_price/avalanche?contract_addresses=${tokenPrice}&vs_currencies=usd`
+    )
     const listTokenPrices = await response.json()
     setlistTokenPrices(listTokenPrices)
   }
 
   async function handleFetchBalance() {
-    const response = await fetch(`${URL_1INCH_BALANCE}/43114/allowancesAndBalances/0x1111111254eeb25477b68fb85ed929f73a960582/0xFdFeC1cbc5A10FC8F69C08af8D91Ea3B5190b5e6?tokensFetchType=listedTokens`)
+    const response = await fetch(
+      `${URL_1INCH_BALANCE}/43114/allowancesAndBalances/0x1111111254eeb25477b68fb85ed929f73a960582/${userWalletAddress}?tokensFetchType=listedTokens`
+    )
     const listTokenBalanceInWallet = await response.json()
     setBalanceToken(listTokenBalanceInWallet)
   }
 
   function handleFiltered(tokenList1Inch: ITokenList1InchProps[]) {
     if (isAddress(searchToken)) {
-      const token1inchFilteredByAddress = tokenList1Inch.filter(token => token.address === searchToken)
+      const token1inchFilteredByAddress = tokenList1Inch.filter(
+        token => token.address === searchToken
+      )
 
-      const token1inchWithBalance: IUserTokenProps[] = token1inchFilteredByAddress.map(token => {
-        const tokenBalance = balanceToken[token.address]?.balance || 0
-        const tokenPriceInDollar = listTokenPrices[token.address]?.usd || 0
-        const balanceTokenFormated = BNtoDecimal((new BigNumber(tokenBalance || 0)), token.decimals, 5, 2)
+      const token1inchWithBalance: IUserTokenProps[] =
+        token1inchFilteredByAddress.map(token => {
+          const tokenBalance = balanceToken[token.address]?.balance || 0
+          const tokenPriceInDollar = listTokenPrices[token.address]?.usd || 0
+          const balanceTokenFormated = BNtoDecimal(
+            new BigNumber(tokenBalance || 0),
+            token.decimals,
+            5,
+            2
+          )
 
-        return {
-          ...token,
-          tokenScore: 0,
-          balance: balanceTokenFormated,
-          balanceInDollar: (Number(balanceTokenFormated) * tokenPriceInDollar).toLocaleString('en-US')
-        }
-      })
+          return {
+            ...token,
+            tokenScore: 0,
+            balance: balanceTokenFormated,
+            balanceInDollar: (
+              Number(balanceTokenFormated) * tokenPriceInDollar
+            ).toLocaleString('en-US')
+          }
+        })
 
-    return token1inchWithBalance
+      return token1inchWithBalance
     }
 
     const tokenFiltered: IUserTokenProps[] = tokenList1Inch
@@ -99,15 +117,21 @@ const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
         const score = stringSimilarity(token.symbol + token.name, searchToken)
         const tokenBalance = balanceToken[token.address]?.balance || 0
         const tokenPriceInDollar = listTokenPrices[token.address]?.usd || 0
-        const balanceTokenFormated = BNtoDecimal((new BigNumber(tokenBalance || 0)), token.decimals, 5, 2)
+        const balanceTokenFormated = BNtoDecimal(
+          new BigNumber(tokenBalance || 0),
+          token.decimals,
+          5,
+          2
+        )
 
         return {
           ...token,
           tokenScore: score,
           balance: balanceTokenFormated,
-          balanceInDollar: (Number(balanceTokenFormated) * tokenPriceInDollar).toLocaleString('en-US')
+          balanceInDollar: (
+            Number(balanceTokenFormated) * tokenPriceInDollar
+          ).toLocaleString('en-US')
         }
-
       })
       .sort((a, b) => {
         if (a.tokenScore > b.tokenScore) return -1
@@ -115,23 +139,32 @@ const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
         return 0
       })
 
-    return userWalletAddress ? tokenFiltered.sort((a, b) => {
-      if (a.balanceInDollar > b.balanceInDollar) return -1
-      if (a.balanceInDollar < b.balanceInDollar) return 1
-      return 0
-    }) : tokenFiltered
+    return userWalletAddress
+      ? tokenFiltered.sort((a, b) => {
+          if (a.balanceInDollar > b.balanceInDollar) return -1
+          if (a.balanceInDollar < b.balanceInDollar) return 1
+          return 0
+        })
+      : tokenFiltered
   }
 
   function FilteredBalance(tokenArray: ITokenList1InchProps[]) {
     const tokenArrayFormated: IUserTokenProps[] = tokenArray.map(token => {
       const tokenBalance = balanceToken[token.address]?.balance || 0
       const tokenPriceInDollar = listTokenPrices[token.address]?.usd || 0
-      const balanceTokenFormated = BNtoDecimal((new BigNumber(tokenBalance || 0)), token.decimals, 5, 2)
+      const balanceTokenFormated = BNtoDecimal(
+        new BigNumber(tokenBalance || 0),
+        token.decimals,
+        5,
+        2
+      )
 
       return {
         ...token,
         balance: balanceTokenFormated,
-        balanceInDollar: (Number(balanceTokenFormated) * tokenPriceInDollar).toLocaleString('en-US')
+        balanceInDollar: (
+          Number(balanceTokenFormated) * tokenPriceInDollar
+        ).toLocaleString('en-US')
       }
     })
 
@@ -143,8 +176,9 @@ const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
   }
 
   const filteredToken =
-    searchToken.length > 1 ? handleFiltered(tokenList1Inch) : FilteredBalance(tokenList1Inch)
-
+    searchToken.length > 1
+      ? handleFiltered(tokenList1Inch)
+      : FilteredBalance(tokenList1Inch)
 
   function handleSearchToken(text: string) {
     setSearchToken(text.trim().toLocaleLowerCase())
@@ -176,7 +210,10 @@ const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
             value={searchToken}
             onChange={event => handleSearchToken(event.target.value)}
           />
-          <S.deleteSearch isShowIcon={searchToken.length > 0} onClick={() => setSearchToken('')}>
+          <S.deleteSearch
+            isShowIcon={searchToken.length > 0}
+            onClick={() => setSearchToken('')}
+          >
             <img src="/assets/utilities/close.svg" alt="" />
           </S.deleteSearch>
         </S.InputContent>
@@ -216,7 +253,7 @@ const TokenSelection = ({ tokenList1Inch }: ITokenSelectionProps) => {
           listBalanceToken={balanceToken}
           filteredToken={filteredToken}
           listTokenPrices={listTokenPrices}
-              />
+        />
       </S.BodyToken>
     </S.TokenSelection>
   )
