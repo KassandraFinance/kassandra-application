c0a1c3c60218b5f132ac292d190ca6ad3e7a7dba
Jony <jonyreiscardoso@gmail.com>
2022-11-29 15:53:40 -0300

    refactor: add handleOnWheel function on InputAndOutputValueToken component

--- src/components/NewPoolOperations/Form/InputAndOutputValueToken/index.tsx
+++ src/components/NewPoolOperations/Form/InputAndOutputValueToken/index.tsx
@@ -25,6 +25,13 @@ const InputAndOutputValueToken = () => {
     state => state
   )
 
+  function handleOnWheel() {
+    if (document.activeElement?.classList.contains("noscroll")) {
+      // eslint-disable-next-line prettier/prettier
+      (document.activeElement as HTMLElement).blur()
+    }
+  }
+
   // get balance of swap in token
   React.useEffect(() => {
     if (
@@ -63,6 +70,14 @@ const InputAndOutputValueToken = () => {
     // swapOutAddress
   ])
 
+  //   disabled={
+  //   userWalletAddress.length === 0
+  //     ? "Please connect your wallet by clicking the button below"
+  //     : chainId !== poolChain.chainId
+  //       ? `Please change to the ${poolChain.chainName} by clicking the button below`
+  //       : ""
+  // }
+
   return (
     <S.InputAndOutputValueToken>
       <S.FlexContainer>
@@ -100,64 +115,67 @@ const InputAndOutputValueToken = () => {
             </S.ButtonMax>
             {/* <Tippy content={disabled} disabled={disabled.length === 0}> */}
             <S.Input
-            // className="noscroll"
-            // readOnly={disabled.length > 0}
-            // ref={inputRef}
-            // // value={inputRef?.current?.value}
-            // type="number"
-            // placeholder="0"
-            // step="any"
-            // onWheel={() => handleOnWheel()}
-            // onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            //   const target = e.target as HTMLInputElement
-            //   // don't allow negative numbers
-            //   if (e.key === '-') {
-            //     e.preventDefault()
-            //   }
-            //   // Blink bug makes the value come empty if pressing the decimal symbol that is not that of the current locale
-            //   else if (e.key === '.' || e.key === ',') {
-            //     // first time value will be ok, if pressing twice it zeroes, we ignore those
-            //     if (target.value.length > 0 && target.value.search(/[,.]/) === -1) {
-            //       target.dataset.lastvalue = target.value
-            //     }
-            //   }
-            //   else if (e.key === 'Backspace' || e.key === 'Delete') {
-            //     target.dataset.lastvalue = '0'
-            //   }
-            // }}
-            // onChange={
-            //   (e: React.ChangeEvent<HTMLInputElement>) => {
-            //     let { value } = e.target
+              className="noscroll"
+              // readOnly={disabled.length > 0}
+              // ref={inputRef}
+              // value={inputRef?.current?.value}
+              type="number"
+              placeholder="0"
+              step="any"
+              onWheel={() => handleOnWheel()}
+              onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                const target = e.target as HTMLInputElement
+                // don't allow negative numbers
+                if (e.key === '-') {
+                  e.preventDefault()
+                }
+                // Blink bug makes the value come empty if pressing the decimal symbol that is not that of the current locale
+                else if (e.key === '.' || e.key === ',') {
+                  // first time value will be ok, if pressing twice it zeroes, we ignore those
+                  if (target.value.length > 0 && target.value.search(/[,.]/) === -1) {
+                    target.dataset.lastvalue = target.value
+                  }
+                }
+                else if (e.key === 'Backspace' || e.key === 'Delete') {
+                  target.dataset.lastvalue = '0'
+                }
+              }}
+              // onChange={
+              //   (e: React.ChangeEvent<HTMLInputElement>) => {
+              //     let { value } = e.target
 
-            //     if (value.length === 0) {
-            //       value = e.target.dataset.lastvalue as string
-            //     }
-            //     else if (value[0] === '0') {
-            //       e.target.value = value.replace(/^0+/, '')
-            //     }
+              //     if (value.length === 0) {
+              //       value = e.target.dataset.lastvalue as string
+              //     }
+              //     else if (value[0] === '0') {
+              //       e.target.value = value.replace(/^0+/, '')
+              //     }
 
-            //     if (e.target.value[0] === '.') {
-            //       e.target.value = `0${e.target.value}`
-            //     }
+              //     if (e.target.value[0] === '.') {
+              //       e.target.value = `0${e.target.value}`
+              //     }
 
-            //     const decimalsNum = decimals.toNumber()
-            //     const values = value.split('.')
-            //     const paddedRight = `${values[0]}${`${values[1] || 0}${'0'.repeat(decimalsNum)}`.slice(0, decimalsNum)
-            //       }`
-            //     setMaxActive && setMaxActive(false)
-            //     setInputValue && setInputValue(new BigNumber(paddedRight))
-            //   }
-            // }
+              //     const decimalsNum = decimals.toNumber()
+              //     const values = value.split('.')
+              //     const paddedRight = `${values[0]}${`${values[1] || 0}${'0'.repeat(decimalsNum)}`.slice(0, decimalsNum)
+              //       }`
+              //     // setMaxActive && setMaxActive(false)
+              //     setInputValue && setInputValue(new BigNumber(paddedRight))
+              //   }
+              // }
             />
             {/* </Tippy> */}
             <span className="price-dolar">
-              {/* {address &&
+              {/* {tokenSelect.address &&
                 amount &&
                 'USD: ' +
                   BNtoDecimal(
                     Big(amount.toString())
-                      .mul(Big(priceDollar(address, poolTokensArray)))
-                      .div(Big(10).pow(Number(decimals))),
+                      .mul(
+                        Big(priceDollar(tokenSelect.address, poolTokensArray))
+                      )
+                      .div(Big(10).pow(Number(18))),
+                    // .div(Big(10).pow(Number(decimals))),
                     18,
                     2,
                     2
