622bc4948119133353baf231dc45ab1132719a0e
tiago <tgnvsctn@gmail.com>
2022-12-05 11:19:03 -0300

    feat: created manage page

---
+++ src/templates/Manage/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Manage = () => {
+  return (
+    <S.Manage>
+      <h1>Ready to create your first pool?</h1>
+    </S.Manage>
+  )
+}
+
+export default Manage
