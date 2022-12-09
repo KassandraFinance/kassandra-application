e4f91d0d5575f7a6e6e66275cea678a3caf6b8f7
ChristanDaniel <christian_daniel04@hotmail.com>
2022-12-09 15:31:14 -0400

    feat: add FundImage and PrivacySettings in PoolSttings component

--- src/templates/Manage/CreatePool/SetDetails/PoolSettings/styles.ts
+++ src/templates/Manage/CreatePool/SetDetails/PoolSettings/styles.ts
@@ -1,5 +1,14 @@
 import styled, { css } from 'styled-components'
 
 export const PoolSettings = styled.div`
-  ${() => css``}
+  ${() => css`
+    max-width: 36rem;
+
+    @media (min-width: 360px) and (max-width: 768px) {
+      display: flex;
+      align-items: flex-start;
+      gap: 2.4rem;
+      max-width: 100%;
+    }
+  `}
 `
