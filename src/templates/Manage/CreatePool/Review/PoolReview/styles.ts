06ff6dec5dd262de37d75f9a7d8e6481a2cd5a58
ChristanDaniel <christian_daniel04@hotmail.com>
2022-12-21 19:26:42 -0400

    feat: createPprivacyAddressList in Review page

--- src/templates/Manage/CreatePool/Review/PoolReview/styles.ts
+++ src/templates/Manage/CreatePool/Review/PoolReview/styles.ts
@@ -120,16 +120,20 @@ export const TvlContainer = styled.div`
   `}
 `
 
-export const PrivacySetting = styled.div`
+export const WrapperPoolPrivacy = styled.div`
   ${({ theme }) => css`
-    /* display: flex; */
-    /* align-items: center; */
-    /* justify-content: space-between; */
-    width: 100%;
     padding: 2.4rem 3.2rem;
 
     background: rgba(255, 255, 255, 0.05);
     border-radius: 0.8rem;
+  `}
+`
+
+export const PoolPrivacyLine = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     > P {
       color: #c4c4c4;
@@ -140,13 +144,95 @@ export const PrivacySetting = styled.div`
       line-height: 1.6rem;
     }
 
-    span {
+    > span {
       color: ${theme.colors.white};
       font-size: ${theme.font.sizes.font24};
       font-weight: ${theme.font.weight.medium};
       line-height: 3.2rem;
     }
   `}
+`
+
+export const WrapperPoolPrivate = styled.div`
+  ${({ theme }) => css`
+    border-top: 0.1rem solid rgba(255, 255, 255, 0.5);
+    padding-top: 1rem;
+    margin-top: 1rem;
+
+    > p {
+      margin-bottom: 1.2rem;
+
+      color: ${theme.colors.white};
+      font-size: ${theme.font.sizes.font14};
+      font-weight: ${theme.font.weight.normal};
+      line-height: 1.6rem;
+      letter-spacing: 0.22em;
+      text-transform: uppercase;
+    }
+  `}
+`
+
+export const PrivateAddressList = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    @media (max-width: 576px) {
+      grid-template-columns: 1fr;
+    }
+  `}
+`
+
+interface IPrivateAddressprops {
+  hasBorder: boolean;
+}
+
+// eslint-disable-next-line prettier/prettier
+export const PrivateAddress = styled.div<IPrivateAddressprops>`
+  ${({ theme, hasBorder }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    padding-bottom: 0.6rem;
+    padding-right: 1.2rem;
+    padding-left: ${hasBorder ? '0' : '1.2rem'};
+
+    :last-child {
+      padding-bottom: 0.2rem;
+    }
+
+    border-right: ${hasBorder
+      ? '0.1rem solid rgba(255, 255, 255, 0.5)'
+      : 'none'};
+
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.font16};
+    font-weight: ${theme.font.weight.light};
+    line-height: 100%;
+
+    svg {
+      cursor: pointer;
+    }
+
+    span,
+    a {
+      display: flex;
+    }
+
+    @media (max-width: 576px) {
+      padding: 0;
+      border: none;
+      margin-bottom: 1rem;
+    }
+  `}
+`
+
+export const WrapperAddressImages = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
 `
 
 export const ReviewTable = styled.div`
