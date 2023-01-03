5974720c83261a78efb81cf1de4be2ee32e32f6a
ChristanDaniel <christian_daniel04@hotmail.com>
2023-01-03 01:32:48 -0400

    refactor: withdraw component

--- src/templates/Pool/NewPoolOperations/Form/Withdraw/index.tsx
+++ src/templates/Pool/NewPoolOperations/Form/Withdraw/index.tsx
@@ -1,7 +1,9 @@
+import Big from 'big.js'
 import React from 'react'
+
 import InputAndOutputValueToken from '../InputAndOutputValueToken'
 import ListOfAllAsset from '../ListOfAllAsset'
-import { TokenAssetIn } from '../TokenAssetIn/styles'
+import TokenAssetIn from '../TokenAssetIn'
 
 import * as S from './styles'
 
@@ -10,11 +12,35 @@ interface IWithdrawProps {
 }
 
 const Withdraw = ({ typeWithdraw }: IWithdrawProps) => {
+  const [amountTokenOut, setAmountTokenOut] = React.useState<Big | string>(
+    Big(0)
+  )
+  const [selectedTokenInBalance, setSelectedTokenInBalance] = React.useState(
+    new Big(-1)
+  )
+  const [errorMsg, setErrorMsg] = React.useState('')
+  const [maxActive, setMaxActive] = React.useState<boolean>(false)
+  const inputAmountTokenRef = React.useRef<HTMLInputElement>(null)
+
   return (
     <S.Withdraw>
       <TokenAssetIn />
-      <ListOfAllAsset />
-      {/* <InputAndOutputValueToken /> */}
+      <img src="/assets/icons/arrow-down.svg" alt="" width={20} height={20} />
+      {typeWithdraw === 'Best_value' ? (
+        <ListOfAllAsset />
+      ) : (
+        <InputAndOutputValueToken
+          typeAction={typeWithdraw}
+          amountTokenIn={amountTokenOut}
+          setAmountTokenIn={setAmountTokenOut}
+          selectedTokenInBalance={selectedTokenInBalance}
+          setSelectedTokenInBalance={setSelectedTokenInBalance}
+          maxActive={maxActive}
+          setMaxActive={setMaxActive}
+          inputAmountTokenRef={inputAmountTokenRef}
+          errorMsg={errorMsg}
+        />
+      )}
     </S.Withdraw>
   )
 }
--- src/templates/Pool/NewPoolOperations/Form/Withdraw/styles.ts
+++ src/templates/Pool/NewPoolOperations/Form/Withdraw/styles.ts
@@ -2,6 +2,22 @@ import styled, { css } from 'styled-components'
 
 export const Withdraw = styled.form`
   ${({ theme }) => css`
-    background-color: ${theme.colors.darkGray};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    max-height: 100%;
+    padding-top: 2rem;
+    padding-right: 3.2rem;
+    padding-bottom: 3.2rem;
+    padding-left: 3.2rem;
+
+    @media (max-width: 500px) {
+      padding: 2rem;
+    }
+
+    > img {
+      margin-block: 1.2rem;
+    }
   `}
 `
