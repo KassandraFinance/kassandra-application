a688bc8dbd2c896bd9f0537e2052557b4c639ef9
ChristanDaniel <christian_daniel04@hotmail.com>
2023-01-04 09:16:50 -0400

    feat: ending tokenAssetIn in withdraw component

--- src/templates/Pool/NewPoolOperations/Form/TokenAssetIn/index.tsx
+++ src/templates/Pool/NewPoolOperations/Form/TokenAssetIn/index.tsx
@@ -1,14 +1,104 @@
 import React from 'react'
 import Tippy from '@tippyjs/react'
+import useSWR from 'swr';
+import Big from 'big.js';
+import { request } from 'graphql-request';
 
 import { useAppSelector } from '../../../../../store/hooks'
+import { ERC20 } from '../../../../../hooks/useERC20Contract';
+
+import { GET_POOL } from './graphql';
+
+import { SUBGRAPH_URL } from '../../../../../constants/tokenAddresses';
+
+import { BNtoDecimal } from '../../../../../utils/numerals';
 
 import * as S from './styles'
 
-const TokenAssetIn = () => {
-  const { pool } = useAppSelector(state => state)
+
+interface ITokenAssetInProps {
+  amountTokenIn: string | Big;
+  setamountTokenIn: React.Dispatch<React.SetStateAction<string | Big>>;
+  maxActive: boolean;
+  setMaxActive: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedTokenInBalance: Big;
+  setSelectedTokenInBalance: React.Dispatch<React.SetStateAction<Big>>;
+  inputAmountTokenRef: React.RefObject<HTMLInputElement>;
+  errorMsg: string;
+}
+
+const TokenAssetIn = ({
+  amountTokenIn,
+  setamountTokenIn,
+  maxActive,
+  setMaxActive,
+  selectedTokenInBalance,
+  setSelectedTokenInBalance,
+  inputAmountTokenRef,
+  errorMsg
+ }: ITokenAssetInProps) => {
+  const { pool, userWalletAddress, chainId } = useAppSelector(state => state)
+
+  const { data } = useSWR([GET_POOL], query =>
+    request(SUBGRAPH_URL, query, { id: pool.id })
+  )
+
+  console.log(data)
 
   const messageError = false
+
+
+  function wei2String(input: Big) {
+    return BNtoDecimal(input.div(Big(10).pow(Number(18))), 18).replace(/\u00A0/g, '')
+  }
+
+  function handleMaxUserBalance() {
+    if (!inputAmountTokenRef || !amountTokenIn) {
+      return
+    }
+
+    if (inputAmountTokenRef.current !== null) {
+      inputAmountTokenRef.current.focus()
+
+      if (maxActive) {
+        inputAmountTokenRef.current.value = ''
+        setamountTokenIn(new Big(0))
+        setMaxActive(false)
+        return
+      }
+
+      inputAmountTokenRef.current.value = wei2String(selectedTokenInBalance)
+      setamountTokenIn(selectedTokenInBalance)
+      setMaxActive(true)
+    }
+  }
+
+  React.useEffect(() => {
+    if (
+      pool.id.length === 0 ||
+      userWalletAddress.length === 0 ||
+      chainId.toString().length === 0 ||
+      chainId !== pool.chainId
+    ) {
+      return
+    }
+    // if (tokenSelect.address === addressNativeToken1Inch) {
+    //   web3.eth
+    //     .getBalance(userWalletAddress)
+    //     .then(newBalance =>
+    //       setSelectedTokenInBalance(Big(newBalance.toString()))
+    //     )
+    //   return
+    // }
+    const token = ERC20(pool.id)
+    token
+      .balance(userWalletAddress)
+      .then(newBalance =>
+        setSelectedTokenInBalance(Big(newBalance.toString()))
+      )
+  }, [userWalletAddress, pool])
+
+  console.log(data?.pool?.price_usd || 0)
 
   return (
     <S.TokenAssetIn>
@@ -23,38 +113,37 @@ const TokenAssetIn = () => {
           </S.Token>
           <S.Span>
             Balance:{' '}
-            {/* {selectedTokenInBalance > new Big(-1)
+            {selectedTokenInBalance > new Big(-1)
               ? BNtoDecimal(
                   selectedTokenInBalance.div(
-                    Big(10).pow(tokenSelect.decimals)
+                    Big(10).pow(18)
                   ),
-                  tokenSelect.decimals
+                  18
                 )
-              : '...'} */}
-              $0,00
+              : '...'}
           </S.Span>
         </S.PoolInfo>
         <S.AmountContainer>
           <S.ButtonMax
             type="button"
-            // maxActive={0}
-            // onClick={() => {
-            //   handleMaxUserBalance()
-            //   trackEventFunction(
-            //     'click-on-maxBtn',
-            //     'input-in-Invest',
-            //     'operations-invest'
-            //   )
-            // }}
+            maxActive={maxActive}
+            onClick={() => {
+              handleMaxUserBalance()
+              // trackEventFunction(
+              //   'click-on-maxBtn',
+              //   'input-in-Invest',
+              //   'operations-invest'
+              // )
+            }}
           >
             Max
           </S.ButtonMax>
-          <Tippy content="{disabled}" disabled={"disabled".length === 0}>
+          {/* <Tippy content="{disabled}" disabled={"disabled".length === 0}> */}
           <S.Input
             className="noscroll"
             // readOnly={disabled.length > 0}
-            // ref={inputAmountTokenRef}
-            // value={inputRef?.current?.value}
+            ref={inputAmountTokenRef}
+            // value={inputAmountTokenRef?.current?.value}
             type="number"
             placeholder="0"
             step="any"
@@ -92,31 +181,31 @@ const TokenAssetIn = () => {
                   e.target.value = `0${e.target.value}`
                 }
 
-                // const decimalsNum = tokenSelect.decimals
+                const decimalsNum = 18
                 const values = value.split('.')
-                // const paddedRight = `${values[0]}${`${values[1] || 0}${'0'.repeat(decimalsNum)}`.slice(0, decimalsNum)}`
+                const paddedRight = `${values[0]}${`${values[1] || 0}${'0'.repeat(decimalsNum)}`.slice(0, decimalsNum)}`
 
-                // setMaxActive(false)
-                // setAmountTokenIn(paddedRight)
+                setMaxActive(false)
+                setamountTokenIn(paddedRight)
               }
             }
           />
-          </Tippy>
+          {/* </Tippy> */}
           <span className="price-dolar">
-          USD: 0,00
-            {/* {tokenSelect.address &&
+          {/* USD: 0,00 */}
+            {pool.id &&
               amountTokenIn &&
               'USD: ' +
                 BNtoDecimal(
                   Big(amountTokenIn.toString())
                     .mul(
-                      Big(priceToken(tokenSelect.address) || 0)
+                      Big(data?.pool?.price_usd || 0)
                     )
-                    .div(Big(10).pow(Number(tokenSelect.decimals))),
+                    .div(Big(10).pow(Number(data?.pool?.decimals || 18))),
                   18,
                   2,
                   2
-                )} */}
+                )}
           </span>
         </S.AmountContainer>
       </S.Body>
