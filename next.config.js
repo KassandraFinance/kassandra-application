c83778648424f561f09e2885526933d2ff6917f3
ChristanDaniel <christian_daniel04@hotmail.com>
2023-01-26 17:31:14 -0400

    feat: add new domains in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['assets.coingecko.com', 'cdn-images-1.medium.com', 'tokens.1inch.io', 'storage.cloud.google.com']
+    domains: ['assets.coingecko.com', 'cdn-images-1.medium.com', 'tokens.1inch.io', 'storage.cloud.google.com', 'storage.googleapis.com']
   }
 }
