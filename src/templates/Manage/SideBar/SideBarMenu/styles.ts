16fdce104a343d5141f9e3bf90bf6841e8343f61
tiago <tgnvsctn@gmail.com>
2023-02-09 18:51:05 -0300

    refactor: changed animation duration

--- src/templates/Manage/SideBar/SideBarMenu/styles.ts
+++ src/templates/Manage/SideBar/SideBarMenu/styles.ts
@@ -46,7 +46,7 @@ export const TitleText = styled.p<ITitleTextProps>`
 
     opacity: 1;
 
-    transition-duration: 700ms;
+    transition-duration: 500ms;
     transition-timing-function: ease;
     transition-property: opacity;
   `}
@@ -78,7 +78,7 @@ export const OpenButton = styled.div<IOpenButtonProps>`
 
     background-color: transparent;
 
-    transition-duration: 550ms;
+    transition-duration: 500ms;
     transition-timing-function: ease;
     transition-property: transform;
   `}
@@ -101,7 +101,7 @@ export const PoolContainer = styled.div<IPoolContainerProps>`
     opacity: 0;
     overflow: hidden;
 
-    transition-duration: 700ms;
+    transition-duration: 500ms;
     transition-timing-function: ease;
     transition-property: max-height opacity;
   `}
