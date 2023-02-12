b2943e15bbba00c3a6ccc0b823a3c0e933f6d523
ChristanDaniel <christian_daniel04@hotmail.com>
2023-02-12 18:01:32 -0400

    feat: finalized ExecutionPeriod component

--- src/templates/Manage/ManageAssets/RebalanceAssets/ExecutionPeriod/index.tsx
+++ src/templates/Manage/ManageAssets/RebalanceAssets/ExecutionPeriod/index.tsx
@@ -1,4 +1,7 @@
+import Tippy from '@tippyjs/react'
 import React from 'react'
+import InputRadio from '../../../../../components/Inputs/InputRadio'
+import InputTime from '../../../../../components/Inputs/InputTime'
 
 import * as S from './styles'
 
@@ -7,9 +10,108 @@ import * as S from './styles'
 // }
 
 const ExecutionPeriod = () => {
+  const [timeValue, setTimeValue] = React.useState<number>()
+  const [timePeriodSelect, setTimePeriodSelect] = React.useState('optimized')
+
+  function handleClickInput(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value
+
+    setTimePeriodSelect(value)
+    console.log('RADIO', value)
+  }
+
+  function handleInputTime(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value
+    const valueNumber = Number(value)
+
+    if (valueNumber !== 0) {
+      setTimeValue(Number(value))
+      setTimePeriodSelect('')
+    } else {
+      setTimeValue(undefined)
+
+      timePeriodSelect === '' && setTimePeriodSelect('optimized')
+    }
+  }
+
   return (
     <S.ExecutionPeriod>
-      <h1>ExecutionPeriod</h1>
+      <h3>token weights rebalancing execution period</h3>
+
+      <S.SelectPeriodCotainer>
+        <S.SelectPeriod>
+          <InputRadio
+            text="Optimized"
+            inputId="optimized"
+            value="optimized"
+            inputChecked={timePeriodSelect === 'optimized'}
+            handleClickInput={handleClickInput}
+          />
+
+          <S.SelectPeriodContent>
+            <p>72 hours</p>
+            <Tippy content="TIPPY">
+              <img src="/assets/utilities/tooltip.svg" />
+            </Tippy>
+          </S.SelectPeriodContent>
+        </S.SelectPeriod>
+        <S.SelectPeriod>
+          <InputRadio
+            text="Average"
+            inputId="average"
+            value="average"
+            inputChecked={timePeriodSelect === 'average'}
+            handleClickInput={handleClickInput}
+          />
+
+          <S.SelectPeriodContent>
+            <p>48 hours</p>
+            <Tippy content="TIPPY">
+              <img src="/assets/utilities/tooltip.svg" />
+            </Tippy>
+          </S.SelectPeriodContent>
+        </S.SelectPeriod>
+        <S.SelectPeriod>
+          <InputRadio
+            text="Fast"
+            inputId="fast"
+            value="fast"
+            inputChecked={timePeriodSelect === 'fast'}
+            handleClickInput={handleClickInput}
+          />
+
+          <S.SelectPeriodContent>
+            <p>24 hours</p>
+            <Tippy content="TIPPY">
+              <img src="/assets/utilities/tooltip.svg" />
+            </Tippy>
+          </S.SelectPeriodContent>
+        </S.SelectPeriod>
+      </S.SelectPeriodCotainer>
+
+      <S.PersonalizePeriodContainer>
+        <span>Personalize</span>
+        <S.PersonalizePeriod>
+          <p>
+            The period for executing the rebalance must be between 24 and 72
+            hours.{' '}
+          </p>
+          <InputTime
+            InputTimeValue={timeValue}
+            handleInputTime={handleInputTime}
+            name="Hour/s"
+            max={72}
+            min={24}
+            step={1}
+          />
+        </S.PersonalizePeriod>
+        {timeValue && (timeValue > 72 || timeValue < 24) && (
+          <S.ErrorPeriod>
+            The amount of time for the rebalancing process must be higher than
+            24 and lower than 72 hours
+          </S.ErrorPeriod>
+        )}
+      </S.PersonalizePeriodContainer>
     </S.ExecutionPeriod>
   )
 }
--- src/templates/Manage/ManageAssets/RebalanceAssets/ExecutionPeriod/styles.ts
+++ src/templates/Manage/ManageAssets/RebalanceAssets/ExecutionPeriod/styles.ts
@@ -1,4 +1,6 @@
 import styled, { css } from 'styled-components'
+import { Label } from '../../../../../components/Inputs/InputRadio/styles'
+import { InputNumber } from '../../../../../components/Inputs/InputRange/styles'
 
 // interface IProps {
 //   isActive: boolean;
@@ -6,5 +8,105 @@ import styled, { css } from 'styled-components'
 // eslint-disable-next-line prettier/prettier
 
 export const ExecutionPeriod = styled.div`
-  ${() => css``}
+  ${({ theme }) => css`
+    background: rgba(255, 255, 255, 0.05);
+    border-radius: 8px;
+    padding: 2.4rem;
+    margin-bottom: 10rem;
+    max-width: 36.1rem;
+
+    > h3 {
+      font-size: ${theme.font.sizes.font14};
+      font-weight: ${theme.font.weight.normal};
+      line-height: 1.6rem;
+      letter-spacing: 0.22em;
+      text-transform: uppercase;
+    }
+  `}
+`
+
+export const SelectPeriodCotainer = styled.div`
+  ${() => css`
+    margin-top: 2.4rem;
+    margin-bottom: 2.4rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+  `}
+`
+
+export const SelectPeriod = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    ${Label} {
+      font-weight: ${theme.font.weight.light};
+      line-height: 104%;
+    }
+  `}
+`
+
+export const SelectPeriodContent = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+
+    > p {
+      color: #fcfcfc;
+      font-size: ${theme.font.sizes.font16};
+      font-weight: ${theme.font.weight.medium};
+      line-height: 110%;
+    }
+  `}
+`
+
+export const PersonalizePeriodContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    padding-top: 2.4rem;
+
+    border-top: 1px solid rgba(255, 255, 255, 0.3);
+
+    span {
+      font-size: ${theme.font.sizes.font14};
+      font-weight: ${theme.font.weight.normal};
+      line-height: 1.6rem;
+      letter-spacing: 0.22em;
+      text-transform: uppercase;
+
+      margin-bottom: 1.6rem;
+    }
+  `}
+`
+
+export const PersonalizePeriod = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    > input {
+      max-width: 6.9rem;
+    }
+
+    P {
+      font-size: ${theme.font.sizes.font16};
+      font-weight: ${theme.font.weight.light};
+      line-height: 135%;
+    }
+  `}
+`
+
+export const ErrorPeriod = styled.p`
+  ${({ theme }) => css`
+    color: #e8372c;
+    font-size: ${theme.font.sizes.font14};
+    font-weight: ${theme.font.weight.light};
+    line-height: 135%;
+  `}
 `
