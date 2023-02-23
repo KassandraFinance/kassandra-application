4c51fcf76acce7cf42aa89f0722756d3ff23d5f6
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-02-23 09:56:35 -0300

    refactor: mockList to get new tokens

--- src/templates/Manage/CreatePool/SelectAssets/index.tsx
+++ src/templates/Manage/CreatePool/SelectAssets/index.tsx
@@ -34,7 +34,7 @@ export const mockTokens: { [key: string]: string } = {
     '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // wmatic
   '0xca813266889e0FD141dF48B85294855616015fA4':
     '0xeeeeeb57642040be42185f49c52f7e9b38f8eeee', // elk
-  '0xb22ED6ED220506E4757Bc90cbB05d41b6257b590':
+  '0xf22f05168508749fa42eDBddE10CB323D87c201d':
     '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // tether
   '0x2f52C8ce1e5A064B4202762aD34E075E8826C252':
     '0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3', // bnb
@@ -42,7 +42,7 @@ export const mockTokens: { [key: string]: string } = {
     '0xb33eaad8d922b1083446dc23f610c2567fb5180f', // uniswap
   '0xB0C30dDFAF159ce47097E4b08A3436fAE8f43a4d':
     '0xd6df932a45c0f255f85145f286ea0b292b21c90b', // aave
-  '0x07Fb45533CC34Cd88D69C57739ceFb77202733E9':
+  '0xBA1C32241Ac77b97C8573c3dbFDe4e1e2A8fc0DF':
     '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6' // wbiticoin
 }
 
