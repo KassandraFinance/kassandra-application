16afb2478fe9f19afeb3062e3c6789d47bfe837b
tiago <tgnvsctn@gmail.com>
2023-02-28 18:37:06 -0300

    feat: created transaction summary

--- src/templates/Manage/ManageAssets/ReviewAddAsset/TransactionSummary/index.tsx
+++ src/templates/Manage/ManageAssets/ReviewAddAsset/TransactionSummary/index.tsx
@@ -1,9 +1,119 @@
 import React from 'react'
+import Image from 'next/image'
+import useSWR from 'swr'
+import { request } from 'graphql-request'
+import Big from 'big.js'
+
+import {
+  BACKEND_KASSANDRA,
+  COINGECKO_API
+} from '../../../../../constants/tokenAddresses'
+import { GET_POOL_TOKENS } from '../../AddLiquidity/graphql'
+
+import { BNtoDecimal } from '../../../../../utils/numerals'
+
+import { useAppSelector } from '../../../../../store/hooks'
+import {
+  GetPoolTokensType,
+  CoinGeckoAssetsResponseType
+} from '../../AddLiquidity/AddLiquidityOperation'
 
 import * as S from './styles'
 
-const ReviewAddAsset = () => {
-  return <S.ReviewAddAsset></S.ReviewAddAsset>
+const TransactionSummary = () => {
+  const token = useAppSelector(state => state.addAsset.token)
+  const newToken = useAppSelector(state => state.addAsset.token)
+  const newTokenLiquidity = useAppSelector(state => state.addAsset.liquidit)
+  const poolId = useAppSelector(state => state.addAsset.poolId)
+
+  const params = {
+    id: poolId
+  }
+
+  const { data } = useSWR<GetPoolTokensType>(
+    [GET_POOL_TOKENS, params],
+    (query, params) => request(BACKEND_KASSANDRA, query, params)
+  )
+
+  const { data: priceData } = useSWR<CoinGeckoAssetsResponseType>(
+    `${COINGECKO_API}/simple/token_price/polygon-pos?contract_addresses=${token.id}&vs_currencies=usd`
+  )
+
+  return (
+    <S.TransactionSummary>
+      <S.Title>Transaction summary</S.Title>
+
+      <S.Container>
+        <S.FlexContainer>
+          <S.ContentTitle>token added</S.ContentTitle>
+
+          <S.ValueContainer>
+            <S.ValueWrapper>
+              <S.Value>{BNtoDecimal(Big(newTokenLiquidity.amount), 2)}</S.Value>
+
+              <S.SecondaryValue>
+                ~$
+                {priceData &&
+                  BNtoDecimal(
+                    Big(newTokenLiquidity.amount).mul(
+                      priceData[token.id.toLowerCase()].usd
+                    ),
+                    2
+                  )}
+              </S.SecondaryValue>
+            </S.ValueWrapper>
+
+            <S.ImageWrapper>
+              <Image src={newToken.image} layout="fill" />
+            </S.ImageWrapper>
+          </S.ValueContainer>
+        </S.FlexContainer>
+
+        <S.FlexContainer>
+          <S.ContentTitle>allocation</S.ContentTitle>
+
+          <S.Value>{Big(newTokenLiquidity.allocation).toFixed(2)}%</S.Value>
+        </S.FlexContainer>
+      </S.Container>
+
+      <S.Line />
+
+      <S.Container>
+        <S.FlexContainer>
+          <S.ContentTitle>received LP (EST.)</S.ContentTitle>
+
+          {data && priceData && (
+            <S.ValueContainer>
+              <S.ValueWrapper>
+                <S.Value>
+                  {BNtoDecimal(
+                    Big(newTokenLiquidity.amount || 0)
+                      .mul(priceData[token.id.toLowerCase()].usd)
+                      .div(data.pool.price_usd),
+                    2
+                  )}
+                </S.Value>
+
+                <S.SecondaryValue>
+                  ~$
+                  {BNtoDecimal(
+                    Big(newTokenLiquidity.amount || 0).mul(
+                      priceData[token.id.toLowerCase()].usd
+                    ),
+                    2
+                  )}
+                </S.SecondaryValue>
+              </S.ValueWrapper>
+
+              <S.ImageWrapper>
+                <Image src={data.pool.logo} layout="fill" />
+              </S.ImageWrapper>
+            </S.ValueContainer>
+          )}
+        </S.FlexContainer>
+      </S.Container>
+    </S.TransactionSummary>
+  )
 }
 
-export default ReviewAddAsset
+export default TransactionSummary
--- src/templates/Manage/ManageAssets/ReviewAddAsset/TransactionSummary/styles.ts
+++ src/templates/Manage/ManageAssets/ReviewAddAsset/TransactionSummary/styles.ts
@@ -2,6 +2,118 @@ import styled, { css } from 'styled-components'
 
 export const TransactionSummary = styled.div`
   ${() => css`
-    margin-bottom: 16rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+
+    width: 100%;
+    height: 30rem;
+    padding: 2.4rem;
+
+    background: rgba(255, 255, 255, 0.05);
+    border-radius: 8px;
+  `}
+`
+
+export const Title = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    font-weight: ${theme.font.weight.normal};
+    font-size: ${theme.font.sizes.font14};
+    line-height: ${theme.font.sizes.font16};
+    letter-spacing: 0.22em;
+    text-transform: uppercase;
+  `}
+`
+
+export const Container = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+
+    padding: 1.6rem;
+
+    background: rgba(31, 31, 31, 0.72);
+    border-radius: 8px;
+  `}
+`
+
+export const Line = styled.span`
+  ${() => css`
+    display: block;
+
+    width: 100%;
+    height: 0.2rem;
+
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 1px;
+  `}
+`
+
+export const FlexContainer = styled.div`
+  ${() => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  `}
+`
+
+export const ContentTitle = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.grayDisabled};
+    font-weight: ${theme.font.weight.normal};
+    font-size: ${theme.font.sizes.font12};
+    line-height: ${theme.font.sizes.font12};
+    letter-spacing: 0.22em;
+    text-transform: uppercase;
+  `}
+`
+
+export const ValueContainer = styled.div`
+  ${() => css`
+    display: grid;
+    grid-template-columns: 1fr 3.2rem;
+    align-items: center;
+    gap: 0.8rem;
+  `}
+`
+
+export const ValueWrapper = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 0.4rem;
+  `}
+`
+
+export const Value = styled.span`
+  ${({ theme }) => css`
+    color: ${theme.colors.snow};
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 20px;
+    text-align: right;
+  `}
+`
+
+export const SecondaryValue = styled.span`
+  ${({ theme }) => css`
+    color: #bfbfbf;
+    font-weight: ${theme.font.weight.light};
+    font-size: ${theme.font.sizes.font14};
+    line-height: 104%;
+  `}
+`
+
+export const ImageWrapper = styled.div`
+  ${() => css`
+    position: relative;
+
+    width: 3.2rem;
+    height: 3.2rem;
+
+    border-radius: 50%;
+    overflow: hidden;
   `}
 `
