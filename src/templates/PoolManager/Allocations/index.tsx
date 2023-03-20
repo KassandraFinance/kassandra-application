ca33096624bc548b089cbda5b1c468fb664a1533
ChristanDaniel <christian_daniel04@hotmail.com>
2023-03-20 11:14:46 -0400

    feat: create AllocationTable component in Allocations tab

--- src/templates/PoolManager/Allocations/index.tsx
+++ src/templates/PoolManager/Allocations/index.tsx
@@ -1,7 +1,11 @@
+import Big from 'big.js'
+
 import { getWeightsNormalizedV2 } from '@/utils/updateAssetsToV2'
 import { underlyingAssetsInfo, WeightsV2 } from '@/store/reducers/pool'
 
 import PieChart from './PieChart'
+import AllocationTable from './AllocationTable'
+
 import * as S from './styles'
 
 interface IAllocationsProps {
@@ -22,8 +26,251 @@ const Allocations = (props: IAllocationsProps) => {
         </S.GridChart>
         <S.GridRebalancing>asdasd</S.GridRebalancing>
       </S.Intro>
+      <AllocationTable allocationData={mockTokens} />
     </S.Allocations>
   )
 }
 
+export const mockTokens = [
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 1,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: -1.25
+    },
+    yields: {
+      apy: '0',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  },
+  {
+    token: {
+      address: 'asd',
+      logo: 'https://assets.coingecko.com/coins/images/28542/thumb/l7jRo-5-_400x400.jpg',
+      symbol: 'WBTC'
+    },
+    allocation: 0.25,
+    holding: {
+      value: Big(101),
+      valueUSD: Big(500)
+    },
+    price: {
+      value: 2.5,
+      changeValue: 1.25
+    },
+    yields: {
+      apy: '50',
+      url: '#'
+    }
+  }
+]
 export default Allocations
