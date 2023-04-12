89746477ec756ccbaa7bef517775224a9dc5ca60
tiago <tgnvsctn@gmail.com>
2023-04-12 14:19:26 -0300

    fix: meta tags test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,22 +16,30 @@ export default function Index({ poolsKassandra }: IIndexProps) {
   return (
     <>
       <Head>
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
-        <meta property="og:image:width" content="1012" />
-        <meta property="og:image:height" content="506" />
-        <meta property="og:url" content="https://kassandra.finance/" />
+        {
+          // Primary Meta Tags
+        }
+        <title>Meta Tags — Preview, Edit and Generate</title>
+        <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dao_kassandra" />
-        <meta name="twitter:title" content="Kassandra" />
-        <meta name="twitter:description" content="Kassandra DAO" />
-        <meta
-          name="twitter:image"
-          content={`https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png`}
-        />
+        {
+        // Open Graph / Facebook
+        }
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        {
+        // Twitter
+        }
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
       </Head>
 
       <Explore poolsKassandra={poolsKassandra} />
