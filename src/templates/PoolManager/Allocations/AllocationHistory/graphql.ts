db76eeea897354af91ca6cba40666cf55f3aedcd
ChristanDaniel <christian_daniel04@hotmail.com>
2023-04-19 16:49:53 -0400

    refactor: move data call from Allocation history to component AllocationHistory

--- src/templates/PoolManager/Allocations/AllocationHistory/graphql.ts
+++ src/templates/PoolManager/Allocations/AllocationHistory/graphql.ts
@@ -1,25 +1,37 @@
 import { gql } from 'graphql-request'
 
-export const GET_TOKENS_POOL = gql`
-  query ($id: ID!) {
+export const GET_ALLOCATION_POOL = gql`
+  query ($id: ID!, $skip: Int) {
     pool(id: $id) {
-      name
-      symbol
-      logo
-      price_usd
-      chain_id
       num_token_add
       num_token_remove
       num_weight_goals
-      chain {
-        blockExplorerUrl
-        addressWrapped
+      num_join
+      num_exit
+      manager {
+        id
       }
-      weight_goals(orderBy: end_timestamp, orderDirection: desc) {
+      weight_goals(
+        orderBy: end_timestamp
+        orderDirection: desc
+        first: 4
+        skip: $skip
+        where: { previous_not: null }
+      ) {
         id
         type
         end_timestamp
         start_timestamp
+        previous {
+          weights {
+            weight_normalized
+            asset {
+              token {
+                symbol
+              }
+            }
+          }
+        }
         token {
           symbol
           logo
