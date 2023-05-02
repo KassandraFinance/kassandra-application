8969ee04883ad2e2002c6d405fa36aea94e6cd91
tiago <tgnvsctn@gmail.com>
2023-05-02 12:36:38 -0300

    fix: header z-index

--- src/components/Modals/ModalFullWindow/styles.ts
+++ src/components/Modals/ModalFullWindow/styles.ts
@@ -3,10 +3,7 @@ import styled, { css } from 'styled-components'
 export const ModalFullWindow = styled.div`
   ${() => css`
     position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
+    inset: 0;
     z-index: 1050;
 
     overflow-y: auto;
