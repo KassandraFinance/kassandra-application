fc4a8dc8a533900ca6edd49b72b55da5ee1c84b1
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-05-25 14:18:24 -0300

    feat: batch request hoot to get balance of many tokens

---
+++ src/hooks/useBatchRequest.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Web3 from 'web3'
+import { AbiItem } from 'web3-utils'
+
+import ERC20ABI from '@/constants/abi/ERC20.json'
+import web3 from '@/utils/web3'
+
+type Provider = Web3
+
+export const useBatchRequest = () => {
+  return React.useMemo(() => {
+    const batchRequestBalance = async (
+      provider: Provider,
+      tokens: string[],
+      userWalltAddress: string
+    ): Promise<Record<string, { balance: string }>> => {
+      const batchRequest = new provider.BatchRequest()
+      const balances = {}
+      for (const token of tokens) {
+        const tokenContract = new provider.eth.Contract(
+          ERC20ABI as unknown as AbiItem,
+          token
+        )
+        batchRequest.add(
+          tokenContract.methods
+            .balanceOf(userWalltAddress)
+            .call.request(
+              { from: userWalltAddress },
+              (_: unknown, balance: string) => {
+                Object.assign(balances, {
+                  [token.toLowerCase()]: { balance }
+                })
+              }
+            )
+        )
+      }
+      batchRequest.execute()
+      const nativeBalance = await web3.eth.getBalance(userWalltAddress)
+      Object.assign(balances, {
+        balance: nativeBalance
+      })
+      return balances
+    }
+
+    return {
+      batchRequestBalance
+    }
+  }, [])
+}
