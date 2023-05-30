bce13121839fb4e9a8e49053ebd69bc761c361b1
tgnvsctn <tgnvsctn@gmail.com>
2023-05-30 19:26:45 -0300

    refactor: userERC20 hook

--- src/hooks/useERC20.tsx
+++ src/hooks/useERC20.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { BrowserProvider, JsonRpcProvider, Contract, MaxUint256 } from 'ethers'
+import {
+  BrowserProvider,
+  JsonRpcProvider,
+  Contract,
+  MaxUint256,
+  ContractTransactionResponse,
+  ErrorCode,
+  ContractTransactionReceipt
+} from 'ethers'
 import { useConnectWallet } from '@web3-onboard/react'
+import { WalletState } from '@web3-onboard/core'
 
 import useTransaction, {
   MessageType,
@@ -14,8 +23,18 @@ type ContractType = {
   send: Contract
 }
 
-function ERC20Contract(contract: ContractType) {
-  const { txNotification, transactionErrors } = useTransaction()
+function ERC20Contract(
+  contract: ContractType,
+  txNotification: (
+    tx: ContractTransactionResponse,
+    message?: MessageType | undefined,
+    callbacks?: CallbacksType | undefined
+  ) => Promise<ContractTransactionReceipt | null>,
+  transactionErrors: (
+    error: unknown,
+    onFail?: (() => void | Promise<void>) | undefined
+  ) => Promise<ErrorCode | undefined>
+) {
   // Read
   const name = async (): Promise<string> => {
     const value = await contract.read.name()
@@ -58,14 +77,14 @@ function ERC20Contract(contract: ContractType) {
     spenderAddress: string,
     message?: MessageType,
     callbacks?: CallbacksType
-  ): Promise<number> => {
+  ): Promise<ContractTransactionReceipt | null> => {
     try {
       const tx = await contract.send.approve(spenderAddress, MaxUint256)
-      const status = await txNotification(tx, message, callbacks)
-      return status
+      const receipt = await txNotification(tx, message, callbacks)
+      return receipt
     } catch (error) {
       transactionErrors(error, callbacks?.onFail)
-      return 0
+      return null
     }
   }
 
@@ -83,7 +102,7 @@ function ERC20Contract(contract: ContractType) {
 
 const useERC20 = (address: string, rpcURL = networks[137].rpc) => {
   const [{ wallet }] = useConnectWallet()
-  // const { txNotification, transactionErrors } = useTransaction()
+  const { txNotification, transactionErrors } = useTransaction()
 
   const readProvider = new JsonRpcProvider(rpcURL)
 
@@ -111,18 +130,50 @@ const useERC20 = (address: string, rpcURL = networks[137].rpc) => {
   }, [address, rpcURL, wallet])
 
   return React.useMemo(() => {
-    return ERC20Contract(contract)
+    return ERC20Contract(contract, txNotification, transactionErrors)
   }, [contract])
 }
+type ParamsType = {
+  wallet: WalletState | null
+  txNotification: (
+    tx: ContractTransactionResponse,
+    message?: MessageType | undefined,
+    callbacks?: CallbacksType | undefined
+  ) => Promise<ContractTransactionReceipt | null>
+  transactionErrors: (
+    error: unknown,
+    onFail?: (() => void | Promise<void>) | undefined
+  ) => Promise<ErrorCode | undefined>
+}
 
-export const ERC20 = (addresses: string, rpcUrl = networks[137].rpc) => {
-  const provider = new JsonRpcProvider(rpcUrl)
+export const ERC20 = (
+  address: string,
+  rpcUrl = networks[137].rpc,
+  params: ParamsType
+) => {
+  const readProvider = new JsonRpcProvider(rpcUrl)
   const contract: ContractType = {
-    read: new Contract(addresses, ERC20ABI, provider),
-    send: new Contract(addresses, ERC20ABI, provider)
+    read: new Contract(address, ERC20ABI, readProvider),
+    send: new Contract(address, ERC20ABI, readProvider)
   }
 
-  return ERC20Contract(contract)
+  async function signContranct(sendProvider: BrowserProvider) {
+    const signer = await sendProvider.getSigner()
+
+    contract.send = new Contract(address, ERC20ABI, signer)
+  }
+
+  if (params.wallet?.provider) {
+    const sendProvider = new BrowserProvider(params.wallet.provider)
+
+    signContranct(sendProvider)
+  }
+
+  return ERC20Contract(
+    contract,
+    params.txNotification,
+    params.transactionErrors
+  )
 }
 
 export default useERC20
