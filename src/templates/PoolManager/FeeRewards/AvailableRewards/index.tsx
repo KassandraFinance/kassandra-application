17ec341a02ea0cf48ffc7a111c231310923f34fa
ChristanDaniel <christian_daniel04@hotmail.com>
2023-05-30 09:39:57 -0400

    refactor: update management studio tabs to use web3onboard

--- src/templates/PoolManager/FeeRewards/AvailableRewards/index.tsx
+++ src/templates/PoolManager/FeeRewards/AvailableRewards/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Big from 'big.js'
+import { useConnectWallet } from '@web3-onboard/react'
 
 import Button from '@/components/Button'
 
-import useManagePool from '@/hooks/useManagePool'
-import { useAppSelector } from '@/store/hooks'
+import useManagePool from '@/hooks/useManagePoolEthers'
 
 import { getDateDiff } from '@/utils/date'
 import { BNtoDecimal } from '@/utils/numerals'
@@ -27,24 +27,29 @@ const AvailableRewards = ({ pool }: Props) => {
     ? getDateDiff(pool.last_harvest * 1000)
     : { string: 'days', value: 0 }
 
-  const userWalletAddress = useAppSelector(state => state.userWalletAddress)
-  const managePool = useManagePool(pool.controller)
+  const [{ wallet }] = useConnectWallet()
+  const managePool = useManagePool(pool.controller, pool.chain_id)
 
   async function handleClaimRewards() {
-    try {
-      await managePool.withdrawAumFees(userWalletAddress)
+    if (!wallet) return
+
+    function handleSuccess() {
       setFeesAum({ kassandra: '0', manager: '0' })
+    }
+
+    try {
+      await managePool.withdrawAumFees(handleSuccess)
     } catch (error) {
       console.log(error)
     }
   }
 
   React.useEffect(() => {
-    if (!pool) return
+    if (!pool || !wallet) return
     const getAvailableAumFee = async () => {
       try {
         const { feesToManager, feesToKassandra } =
-          await managePool.getAumFeesToManagerAndKassandra(userWalletAddress)
+          await managePool.getAumFeesToManagerAndKassandra()
 
         setFeesAum({ kassandra: feesToKassandra, manager: feesToManager })
       } catch (error) {
@@ -52,7 +57,7 @@ const AvailableRewards = ({ pool }: Props) => {
       }
     }
     getAvailableAumFee()
-  }, [managePool, userWalletAddress])
+  }, [managePool, wallet])
 
   return (
     <S.AvailableRewards>
