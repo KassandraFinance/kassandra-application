096032d673d16aaa00fb27f6ec964ed7a54b8211
Tiago <tgnvsctn@gmail.com>
2023-07-03 15:02:27 -0300

    feat: configured codegen to use kassandra backend

--- codegen.ts
+++ codegen.ts
@@ -1,22 +1,22 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
-import { BACKEND_KASSANDRA, SUBGRAPH_URL } from './src/constants/tokenAddresses'
+import { SUBGRAPH_URL } from './src/constants/tokenAddresses'
 
 const config: CodegenConfig = {
   ignoreNoDocuments: true,
   hooks: { afterAllFileWrite: ['eslint --fix'] },
   generates: {
-    // './src/gql/generated/kassandraApi.ts': {
-    //   schema: BACKEND_KASSANDRA,
-    //   documents: ['./src/gql/queries/kassandra/**/*-kassandra.gql'],
-    //   plugins: [
-    //     'typescript',
-    //     'typescript-operations',
-    //     'typescript-graphql-request'
-    //   ],
-    //   config: {
-    //     enumsAsTypes: true
-    //   }
-    // },
+    './src/gql/generated/kassandraApi.ts': {
+      schema: 'https://backend.kassandra.finance/',
+      documents: ['./src/gql/queries/kassandra/**/*-kassandra.gql'],
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-graphql-request'
+      ],
+      config: {
+        enumsAsTypes: true
+      }
+    },
     './src/gql/generated/subgraphApi.ts': {
       schema: SUBGRAPH_URL,
       documents: ['./src/gql/queries/subgraph/**/*-subgraph.gql'],
