b16cb1406eb7e603d0ab11d0695fc4fe511b2cdc
Tiago <tgnvsctn@gmail.com>
2023-07-05 17:20:35 -0300

    refactor: removed fonts from publick

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,11 +63,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
                   />
 
                   <link rel="manifest" href="/manifest.json" />
-                  <link rel="preconnect" href="https://fonts.gstatic.com" />
-                  <link
-                    href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&amp;display=swap"
-                    rel="stylesheet"
-                  />
                   <link rel="icon" href="/favicon.ico" />
                   <link
                     rel="icon"
