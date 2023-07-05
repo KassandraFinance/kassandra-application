5c925da60bb2a8582f03d7c3c6d02211beaf540d
ChristanDaniel <christian_daniel04@hotmail.com>
2023-07-05 18:32:14 -0400

    refactor: create getPricePoolLP function in usePriceLP hook

--- src/hooks/usePriceLPEthers.tsx
+++ src/hooks/usePriceLPEthers.tsx
@@ -7,6 +7,7 @@ import { ERC20 } from '@/hooks/useERC20'
 import { VAULT_POLYGON, networks } from '@/constants/tokenAddresses'
 import PriceLP from '@/constants/abi/PriceLP.json'
 import VAULT from '@/constants/abi/VaultBalancer.json'
+import { LpPoolProps, LpPoolType } from '@/constants/pools'
 
 const usePriceLP = (chainId: number) => {
   const provider = new JsonRpcProvider(networks[chainId].rpc)
@@ -50,20 +51,19 @@ const usePriceLP = (chainId: number) => {
       addressDaiAvax: string,
       getPriceLP: boolean
     ) => {
-      const reservesKacyAvax = await getReserves(addressKacyAvax)
-
+      const reservesKacyAvax = await getReserves(addressKacyAvax) //
       const reservesDaiAvax = await getReserves(addressDaiAvax)
 
       let daiReserve = reservesDaiAvax._reserve1
       let avaxReserve = reservesDaiAvax._reserve0
       let kacyReserve = reservesKacyAvax._reserve1
-      let avaxKacyReserve = reservesKacyAvax._reserve0
+      let avaxKacyReserve = reservesKacyAvax._reserve0 //
 
       if (process.env.NEXT_PUBLIC_MASTER !== '1') {
         daiReserve = reservesDaiAvax._reserve0
         avaxReserve = reservesDaiAvax._reserve1
         kacyReserve = reservesKacyAvax._reserve0
-        avaxKacyReserve = reservesKacyAvax._reserve1
+        avaxKacyReserve = reservesKacyAvax._reserve1 //
       }
 
       const avaxInDollar = Big(daiReserve).div(Big(avaxReserve))
@@ -92,11 +92,55 @@ const usePriceLP = (chainId: number) => {
       return { kacyPriceInDollar }
     }
 
+    const getReservesAvax = async (address: string, chainId: number) => {
+      const provider = new JsonRpcProvider(networks[chainId].rpc)
+      const contract = new Contract(address, PriceLP.abi, provider)
+      const value = await contract.getReserves()
+
+      return Big(value?._reserve0 ?? 0)
+    }
+    const getReservesBalancer = async (chainId: number) => {
+      const provider = new JsonRpcProvider(networks[137].rpc)
+      const vault = new Contract(VAULT_POLYGON, VAULT, provider)
+      const res = await vault.getPoolTokenInfo(
+        '0xfaf3bc722d34146be83a2aac40b43148a51a9126000200000000000000000b4c',
+        '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+      )
+
+      return Big(res.cash ?? 0)
+    }
+
+    const getPricePoolLP = async (
+      tokenPoolPrice: Big,
+      poolLPInfo?: LpPoolProps
+    ) => {
+      if (!poolLPInfo) return '0'
+
+      const ReserveValue =
+        poolLPInfo.type === LpPoolType.AVAX
+          ? await getReservesAvax(poolLPInfo.address, poolLPInfo.chainId)
+          : await getReservesBalancer(poolLPInfo.chainId)
+
+      const totalAvaxInDollars = Big(ReserveValue).mul(tokenPoolPrice)
+
+      const ERC20Contract = await ERC20(
+        poolLPInfo.address,
+        networks[poolLPInfo.chainId].rpc
+      )
+      const supplyLPToken = await ERC20Contract.totalSupply()
+
+      return totalAvaxInDollars
+        .mul(2)
+        .div(Big(supplyLPToken.toString()))
+        .toFixed()
+    }
+
     return {
       getContract,
       getReserves,
       getPriceKacyAndLP,
-      getPriceKacyAndLPBalancer
+      getPriceKacyAndLPBalancer,
+      getPricePoolLP
     }
   }, [])
 }
