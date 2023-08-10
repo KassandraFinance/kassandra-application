c1fa1d980432c4a33501b660f5e42548e1581d98
Tiago <tgnvsctn@gmail.com>
2023-08-10 12:16:34 -0300

    refactor: configured distribution table mobile

--- src/templates/Pool/Distribution/index.tsx
+++ src/templates/Pool/Distribution/index.tsx
@@ -38,7 +38,8 @@ import {
   TableLine,
   TableLineTitle,
   ValueContainer,
-  Value as V
+  Value as V,
+  SecondaryValue
 } from '@ui/Modals/ModalViewCoin/styles'
 
 const Distribution = () => {
@@ -57,6 +58,15 @@ const Distribution = () => {
     logo: '',
     name: '',
     address: ''
+  })
+  const [tokenMobile, setTokenMobile] = React.useState({
+    allocation: '',
+    holdingValue: '0',
+    holdingAmount: '0',
+    price: '0',
+    change: 0,
+    yield: 'no Yield',
+    id: ''
   })
 
   const { convertBalanceYRTtoWrap } = useYieldYakEthers()
@@ -137,12 +147,97 @@ const Distribution = () => {
     })
   }
 
-  function handleView() {
-    setTokenData({
-      logo: '',
-      name: '',
-      address: ''
-    })
+  function handleView(token: {
+    __typename?: 'Asset' | undefined
+    balance: string
+    weight_normalized: string
+    weight_goal_normalized: string
+    token: {
+      __typename?: 'Token' | undefined
+      id: string
+      name: string
+      logo?: string | null | undefined
+      symbol: string
+      decimals: number
+      is_wrap_token: number
+      wraps?:
+        | {
+            __typename?: 'Token' | undefined
+            id: string
+            decimals: number
+            symbol: string
+            name: string
+            logo?: string | null | undefined
+          }
+        | null
+        | undefined
+    }
+  }) {
+    if (token.token.is_wrap_token === 0) {
+      setTokenData({
+        logo: token.token.logo || '',
+        name: token.token.symbol,
+        address: ''
+      })
+      setTokenMobile({
+        allocation: (Number(token.weight_normalized) * 100).toFixed(2) || '0',
+        holdingValue: BNtoDecimal(
+          Big(token.balance || 0).times(
+            Big(data?.[token.token.id.toLowerCase()]?.usd || 0)
+          ),
+          18,
+          5,
+          2
+        ),
+        holdingAmount: BNtoDecimal(Big(token.balance || '0'), 18, 5),
+        price: BNtoDecimal(
+          Big(data?.[token.token.id.toLowerCase()]?.usd || 0),
+          18,
+          5,
+          2
+        ),
+        change:
+          data?.[token.token.id.toLowerCase()]?.pricePercentageChangeIn24h || 0,
+        yield: 'no Yield',
+        id: ''
+      })
+    } else {
+      setTokenData({
+        logo: token.token.wraps?.logo || '',
+        name: token.token.wraps?.symbol || '',
+        address: ''
+      })
+      setTokenMobile({
+        allocation: (Number(token.weight_normalized) * 100).toFixed(2) || '0',
+        holdingValue: BNtoDecimal(
+          (balanceYY?.[token.token.id] || Big(0)).times(
+            Big(data?.[token.token.wraps?.id?.toLowerCase() || '']?.usd || 0)
+          ),
+          18,
+          5,
+          2
+        ),
+        holdingAmount: BNtoDecimal(
+          balanceYY?.[token.token.id] || Big(0),
+          18,
+          5
+        ),
+        price: BNtoDecimal(
+          Big(data?.[token.token.wraps?.id.toLowerCase() || '']?.usd || 0),
+          18,
+          5,
+          2
+        ),
+        change:
+          data?.[token.token.wraps?.id.toLowerCase() || '']
+            ?.pricePercentageChangeIn24h || 0,
+        yield:
+          token.token.symbol === 'YRT'
+            ? `${infoDataYY?.[token.token.id]?.apy || '0.0'}% APY`
+            : 'no Yield',
+        id: token.token.symbol === 'YRT' ? token.token.id : ''
+      })
+    }
     setIsOpen(true)
   }
 
@@ -291,7 +386,7 @@ const Distribution = () => {
                   <TD isView={inViewCollum === 4}>
                     <Value>no Yield</Value>
                   </TD>
-                  <TD onClick={() => handleView()}>
+                  <TD onClick={() => handleView(token)}>
                     <ViewButton type="button">
                       <Image src={eyeShowIcon} />
                     </ViewButton>
@@ -430,7 +525,7 @@ const Distribution = () => {
                     <Value>no Yield</Value>
                   )}
                 </TD>
-                <TD onClick={() => handleView()}>
+                <TD onClick={() => handleView(token)}>
                   <ViewButton type="button">
                     <Image src={eyeShowIcon} />
                   </ViewButton>
@@ -446,7 +541,73 @@ const Distribution = () => {
           onClick={() => setIsOpen(false)}
         >
           <TableLine>
-            <TableLineTitle>teste</TableLineTitle>
+            <TableLineTitle>Allocation</TableLineTitle>
+
+            <ValueContainer>
+              <V>{tokenMobile.allocation}%</V>
+            </ValueContainer>
+          </TableLine>
+
+          <TableLine>
+            <TableLineTitle>Holding</TableLineTitle>
+
+            <ValueContainer>
+              <V>$ {tokenMobile.holdingValue}</V>
+
+              <SecondaryValue>{tokenMobile.holdingAmount}</SecondaryValue>
+            </ValueContainer>
+          </TableLine>
+
+          <TableLine>
+            <TableLineTitle>Price 24h</TableLineTitle>
+
+            <ValueContainer>
+              <V>$ {tokenMobile.price}</V>
+
+              <SecondaryValue value={tokenMobile.change}>
+                {tokenMobile.change.toFixed(2)}%
+              </SecondaryValue>
+            </ValueContainer>
+          </TableLine>
+
+          <TableLine>
+            <TableLineTitle>Yield</TableLineTitle>
+
+            <ValueContainer>
+              <V>{tokenMobile.yield}</V>
+
+              {tokenMobile.id.length > 0 ? (
+                <S.YieldYakContent
+                  href={`https://yieldyak.com/farms/detail/${tokenMobile.id}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    trackEventFunction(
+                      'click-on-link',
+                      'yield-pool',
+                      'distribution'
+                    )
+                  }
+                >
+                  <SecondaryValue>Yield Yak</SecondaryValue>
+
+                  <svg
+                    width="12"
+                    height="13"
+                    viewBox="0 0 12 13"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M7.15137 1.39096C7.15137 1.09737 7.40876 0.859375 7.72628 0.859375L11.1757 0.859375C11.4933 0.859375 11.7507 1.09737 11.7507 1.39096V4.58049C11.7507 4.87408 11.4933 5.11208 11.1757 5.11208C10.8582 5.11208 10.6008 4.87408 10.6008 4.58049V2.77755L4.67968 8.2525C4.45516 8.4601 4.09115 8.4601 3.86663 8.2525C3.64211 8.0449 3.64211 7.70832 3.86663 7.50072L9.89942 1.92255H7.72628C7.40876 1.92255 7.15137 1.68455 7.15137 1.39096ZM1.97474 3.83564C1.82226 3.83564 1.67603 3.89164 1.56821 3.99133C1.46039 4.09103 1.39982 4.22624 1.39982 4.36722L1.39982 10.2147C1.39982 10.3557 1.46039 10.4909 1.56821 10.5906C1.67603 10.6903 1.82226 10.7463 1.97474 10.7463H8.29877C8.45124 10.7463 8.59748 10.6903 8.70529 10.5906C8.81311 10.4909 8.87368 10.3557 8.87368 10.2147V7.02516C8.87368 6.73158 9.13108 6.49358 9.44859 6.49358C9.76611 6.49358 10.0235 6.73158 10.0235 7.02516V10.2147C10.0235 10.6376 9.84179 11.0433 9.51834 11.3424C9.19489 11.6414 8.7562 11.8095 8.29877 11.8095H1.97474C1.51731 11.8095 1.07861 11.6414 0.755163 11.3424C0.431713 11.0433 0.25 10.6376 0.25 10.2147L0.25 4.36722C0.25 3.94427 0.431713 3.53863 0.755163 3.23956C1.07861 2.94048 1.51731 2.77246 1.97474 2.77246H5.42421C5.74172 2.77246 5.99912 3.01046 5.99912 3.30405C5.99912 3.59764 5.74172 3.83564 5.42421 3.83564H1.97474Z"
+                      fill="#969696"
+                    />
+                  </svg>
+                </S.YieldYakContent>
+              ) : null}
+            </ValueContainer>
           </TableLine>
         </ModalViewCoin>
       </S.DistributionTable>
