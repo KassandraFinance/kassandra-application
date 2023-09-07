3c53e2280f5b242ad6d8ef29f26481f132c97747
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-09-07 16:36:56 -0300

    refactor: ensure use of contract addresses that are on the networks

--- src/hooks/usePriceLPEthers.tsx
+++ src/hooks/usePriceLPEthers.tsx
@@ -5,7 +5,7 @@ import Big from 'big.js'
 import { lpPoolType } from '@/constants/pools'
 import PriceLP from '@/constants/abi/PriceLP.json'
 import VAULT from '@/constants/abi/VaultBalancer.json'
-import { VAULT_POLYGON, networks } from '@/constants/tokenAddresses'
+import { networks } from '@/constants/tokenAddresses'
 
 import { ERC20 } from '@/hooks/useERC20'
 
@@ -44,8 +44,9 @@ const usePriceLP = () => {
 
       let value
       try {
-        const provider = new JsonRpcProvider(networks[chainId].rpc)
-        const vault = new Contract(VAULT_POLYGON, VAULT, provider)
+        const network = networks[chainId]
+        const provider = new JsonRpcProvider(network.rpc)
+        const vault = new Contract(network.vault, VAULT, provider)
         const res = await vault.getPoolTokenInfo(
           balancerPoolId,
           tokenPoolAddress
