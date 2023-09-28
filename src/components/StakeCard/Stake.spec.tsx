86b56b899b27bc74d62e3e84007d1f22457bb606
ChristanDaniel <christian_daniel04@hotmail.com>
2023-09-28 17:50:58 -0400

    feat: add new test in stakeCard

--- src/components/StakeCard/Stake.spec.tsx
+++ src/components/StakeCard/Stake.spec.tsx
@@ -36,7 +36,53 @@ describe.skip('should StakeFarm component', () => {
     expect(result).toEqual(Big(0))
   })
 
-  it('should re-render component when state is updated', () => {
+  it('handle apr calculation with rewardRate undefined', () => {
+    const data = {
+      kacyPrice: Big('0.05'),
+      poolPrice: Big('8.36'),
+      rewardRate: undefined as unknown as Big,
+      totalDeposit: Big('1372541966384482762331')
+    }
+    const result = handleCalcAPR(data)
+    expect(result).toEqual(Big(0))
+  })
+
+  it('handle apr calculation with totalDeposit undefined', () => {
+    const data = {
+      kacyPrice: Big('0.05'),
+      poolPrice: Big('8.36'),
+      rewardRate: Big('388888888888888876800'),
+      totalDeposit: undefined as unknown as Big
+    }
+    const result = handleCalcAPR(data)
+    expect(result).toEqual(Big(0))
+  })
+
+  it('handle apr calculation with poolPrice undefined', () => {
+    const data = {
+      kacyPrice: Big('0.05'),
+      poolPrice: undefined as unknown as Big,
+      rewardRate: Big('388888888888888876800'),
+      totalDeposit: Big('1372541966384482762331')
+    }
+
+    const result = handleCalcAPR(data)
+    expect(result).toEqual(Big(0))
+  })
+
+  it('handle apr calculation with kacyPrice undefined', () => {
+    const data = {
+      kacyPrice: undefined as unknown as Big,
+      poolPrice: Big('8.36'),
+      rewardRate: Big('388888888888888876800'),
+      totalDeposit: Big('1372541966384482762331')
+    }
+
+    const result = handleCalcAPR(data)
+    expect(result).toEqual(Big(0))
+  })
+
+  it('APR renders correctly', () => {
     render(
       <StakeCard kacyPrice={Big(1)} pool={poolsFunds[0]} poolPrice={Big(1)} />
     )
