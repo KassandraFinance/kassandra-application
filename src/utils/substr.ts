f63e2c22b959f83c2c18c9eb4426d2d395f450e4
ChristanDaniel <christian_daniel04@hotmail.com>
2024-02-20 20:20:32 -0400

    refactor: improve substr function

--- src/utils/substr.ts
+++ src/utils/substr.ts
@@ -1,8 +1,8 @@
-function substr(address: string) {
+function substr(address: string, value = 4) {
   return (
-    address.substring(0, 4) +
+    address.substring(0, value) +
     '....' +
-    address.substring(address.length - 4, address.length)
+    address.substring(address.length - value, address.length)
   )
 }
 
