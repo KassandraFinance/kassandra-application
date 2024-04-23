8c8236ebccabcf0923388afa3165fb8ad68723ad
ChristanDaniel <christian_daniel04@hotmail.com>
2024-04-23 14:57:00 -0400

    refactor: skeleton loading components

--- src/components/SkeletonLoading/index.tsx
+++ src/components/SkeletonLoading/index.tsx
@@ -3,23 +3,32 @@ import React from 'react'
 import * as S from './styles'
 
 interface ILoadingProps {
-  width?: number
+  width?: number | string
   line?: number
   height?: number
   spacings?: number
+  borderRadios?: number
 }
 
 const SkeletonLoading = ({
   height = 2,
   line = 1,
   spacings = 2.4,
-  width
+  width,
+  borderRadios = 4,
+  ...rest
 }: ILoadingProps) => (
   <S.SkeletonLoading spacings={spacings}>
     {Array(line)
       .fill(line)
       .map((_, index) => (
-        <S.SkeletonLoadingContent key={index} height={height} width={width} />
+        <S.SkeletonLoadingContent
+          key={index}
+          height={height}
+          width={width}
+          borderRadios={borderRadios}
+          {...rest}
+        />
       ))}
   </S.SkeletonLoading>
 )
